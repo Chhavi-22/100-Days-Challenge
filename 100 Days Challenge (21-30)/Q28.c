3929e0c9ebdd01d98441a4db247a24c1e89bf71a
Chhavi-22 <sonusagatani@gmail.com>
2025-09-23 01:18:33 +0530

    Add files via upload

---
+++ 100 Days Challenge (21-30)/Q28.c
@@ -0,0 +1,25 @@
+//Q28 (Loops without Arrays/Strings)
+//Write a program to print the product of even numbers from 1 to n.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;   
+    int hasEven = 0;         
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product *= i;
+        hasEven = 1;
+    }
+
+    if (hasEven) {
+        printf("Product of even numbers from 1 to %d = %lld\n", n, product);
+    } else {
+        printf("No even numbers in the range 1 to %d\n", n);
+    }
+
+    return 0;
+}
---
+++ 100 Days Challenge (21-30)/Q30.c
@@ -0,0 +1,23 @@
+//Q30 (Loops without Arrays/Strings)
+//Write a program to reverse a given number.
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int original = num; 
+    
+    while (num != 0) {
+        remainder = num % 10;               
+        reversed = reversed * 10 + remainder; 
+        num /= 10;                          
+    }
+
+    printf("Original number = %d\n", original);
+    printf("Reversed number = %d\n", reversed);
+
+    return 0;
+}
