c9d50de0a3a5af81435cdac946e19dd010917b7f
Chhavi-22 <sonusagatani@gmail.com>
2025-09-23 00:36:48 +0530

    Add files via upload

---
+++ 100 Days Challenge (1-10)/Q6.c
@@ -0,0 +1,23 @@
+//Q6 (User Inputs, Operations & Output)
+//Write a program to swap two numbers using a third variable.
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
